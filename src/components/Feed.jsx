2f7b7cd60cf2db474f6894efdab39bc8fe60207c
Adrian0079 <jorgeadrian.ing77@gmail.com>
2023-11-16 19:55:14 -0600

    V5_Responsive2

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -13,15 +13,33 @@ import fim2 from '../images/fim2.png'
 import fim3 from '../images/cobra.jpg'
 import { Bookmark, BookmarkBorder, ChatBubbleOutline, FavoriteBorder, Send, SentimentDissatisfied, SentimentSatisfiedAlt } from '@mui/icons-material'
 import Stories from './Stories'
+import { ThemeProvider } from '@emotion/react'
+import styled from 'styled-components'
 
 
 const Feed = () => {
+
+  // const  CBox = styled(Box)(({ theme }) => ({
+    
+  // transition: 'width 0.3s ease-in-out', // Transición suave al cambiar el ancho
+  // '& *': {
+  //   transition: 'transform 0.3s ease-in-out', // Transición suave del contenido al escalar
+  // },
+  // '&:hover': {
+  //   width: '100px', // Ancho reducido al hacer hover (puedes cambiar esto a tu necesidad)
+  // },
+  // '&:hover *': {
+  //   transform: 'scale(0.8)', // Escala del contenido al hacer hover (ajusta el valor según sea necesario)
+  // },
+    
+  // }))
 
   const theme1 = createTheme({
     breakpoints: {
       values: {
         xs: 0,
         sm: 600,
+        rs: 718,
         md: 900,
         dmd: 1160,
         lg: 1263,
@@ -29,49 +47,83 @@ const Feed = () => {
       },
     },
   });
-  // flex={4.9}
+  // flex={4.9} md:56, dmd:56, lg:56, xl:56
   return (
-    <Box  position="inherit">
-      <Stack  direction="row" border={0} mt={7} justifyContent="flex-end"
-        spacing={2.8} mr={6}
+    <ThemeProvider theme={theme1}>
+    <Box border={0}>
+      <Stack  direction="row" border={0} mt={7} 
+        spacing={2.8} sx={{
+          justifyContent:{xs:"flex-start", sm:"flex-start", rs:"flex-end", md:"flex-end", dmd:"flex-end", lg:"flex-end", xl:"flex-end"},
+          marginRight: {xs:0, sm:0, rs:6, md:6, dmd:6, lg:6, xl:6}}}
       >
         <Box border={0} justifyContent="center" textAlign="center">
-          <Avatar alt='' src={pe1} sx={{ height:56, width: 56, border:"2px #ff0c60 solid"}}/>
+          <Avatar alt='' src={pe1} 
+          sx={{ 
+          height:{xs:40, sm:40, rs:56, md:56, dmd:56, lg:56, xl:56 }, 
+          width: {xs:40, sm:40, rs:56, md:56, dmd:56, lg:56, xl:56}, 
+          border:"2px #ff0c60 solid"}}/>
           <Typography color="initial" alignItems="center" fontSize={12.5}>jose.rt</Typography>
         </Box>
         <Box border={0} justifyContent="center" textAlign="center">
-          <Avatar alt='' src={pe2} sx={{ height:56, width: 56, border:"2px #ff0c60 solid"}}/>
+          <Avatar alt='' src={pe2} sx={{ 
+          height:{xs:40, sm:40, rs:56, md:56, dmd:56, lg:56, xl:56 }, 
+          width: {xs:40, sm:40, rs:56, md:56, dmd:56, lg:56, xl:56},
+          border:"2px #ff0c60 solid"}}/>
           <Typography color="initial" alignItems="center" fontSize={12.5}>luisa.ew</Typography>
         </Box>
         <Box border={0} justifyContent="center" textAlign="center">
-        <Avatar alt='' src={pe3} sx={{ height:56, width: 56, border:"2px #ff0c60 solid"}}/>
+        <Avatar alt='' src={pe3} sx={{
+          height:{xs:40, sm:40, rs:56, md:56, dmd:56, lg:56, xl:56 }, 
+          width: {xs:40, sm:40, rs:56, md:56, dmd:56, lg:56, xl:56},
+          border:"2px #ff0c60 solid"}}/>
         <Typography color="initial" alignItems="center" fontSize={12.5}>marcc.6t</Typography>
         </Box>
         <Box border={0} justifyContent="center" textAlign="center">
-        <Avatar alt='' src={pe4} sx={{ height:56, width: 56, border:"2px #ff0c60 solid"}}/>
+        <Avatar alt='' src={pe4} sx={{
+          height:{xs:40, sm:40, rs:56, md:56, dmd:56, lg:56, xl:56 }, 
+          width: {xs:40, sm:40, rs:56, md:56, dmd:56, lg:56, xl:56},
+          border:"2px #ff0c60 solid"}}/>
         <Typography color="initial" alignItems="center" fontSize={12.5}>dann.54</Typography>
         </Box>
         <Box border={0} justifyContent="center" textAlign="center"> 
-        <Avatar alt='' src={pe5} sx={{ height:56, width: 56, border:"2px #ff0c60 solid"}}/>
+        <Avatar alt='' src={pe5} sx={{ 
+          height:{xs:40, sm:40, rs:56, md:56, dmd:56, lg:56, xl:56 }, 
+          width: {xs:40, sm:40, rs:56, md:56, dmd:56, lg:56, xl:56},
+          border:"2px #ff0c60 solid"}}/>
         <Typography color="initial" alignItems="center" fontSize={12.5}>oc.07</Typography>
         </Box>
         <Box border={0} justifyContent="center" textAlign="center">
-        <Avatar alt='' src={pe6} sx={{ height:56, width: 56, border:"2px #ff0c60 solid"}}/>
+        <Avatar alt='' src={pe6} sx={{ 
+          height:{xs:40, sm:40, rs:56, md:56, dmd:56, lg:56, xl:56 }, 
+          width: {xs:40, sm:40, rs:56, md:56, dmd:56, lg:56, xl:56},
+          border:"2px #ff0c60 solid"}}/>
         <Typography color="initial" alignItems="center" fontSize={12.5}>dog7s</Typography>
         </Box>
         <Box border={0} justifyContent="center" textAlign="center">
-        <Avatar alt='' src={pe7} sx={{ height:56, width: 56, border:"2px #ff0c60 solid"}}/>
+        <Avatar alt='' src={pe7} sx={{
+          height:{xs:40, sm:40, rs:56, md:56, dmd:56, lg:56, xl:56 }, 
+          width: {xs:40, sm:40, rs:56, md:56, dmd:56, lg:56, xl:56},
+          border:"2px #ff0c60 solid"}}/>
         <Typography color="initial" alignItems="center" fontSize={12.5}>mr11</Typography>
         </Box>
         <Box border={0} justifyContent="center" textAlign="center">
-        <Avatar alt='' src={pe8} sx={{ height:56, width: 56, border:"2px #ff0c60 solid"}}/>
+        <Avatar alt='' src={pe8} sx={{
+          height:{xs:40, sm:40, rs:56, md:56, dmd:56, lg:56, xl:56 }, 
+          width: {xs:40, sm:40, rs:56, md:56, dmd:56, lg:56, xl:56},
+          border:"2px #ff0c60 solid"}}/>
         <Typography color="initial" alignItems="center" fontSize={12.5}>af3</Typography>
         </Box>
         
       </Stack>
-      <Stack border={0} height="100%" width="100%"  alignItems="flex-end" mt={5.1} direction="column">
+      <Stack border={0} height="100%" width="100%"  
+      alignItems="flex-end" mt={5.1} direction="column"
+      >
         {/* Card======================> */}
-        <Stack border={0} height="50rem" width={470} mr={17.4} boxShadow={0}>
+        <Stack border={0} height="50rem" width={470} boxShadow={0}
+        sx={{marginRight: {xs:0, sm:0, rs:17.4, md:17.4, dmd:17.4, lg:17.4, xl:17.4}}
+          
+      }
+        >
 
           <Stack border={0} height={56} justifyContent="flex-start" direction="row">
             
@@ -239,6 +291,7 @@ const Feed = () => {
 
       
     </Box>
+    </ThemeProvider>
   )
 }
 
